60f63ad5dceadc439fa41673aec817bfd79d17e7
tenny1225 <xz19910725@gmail.com>
2025-04-27 10:42:21 +0800

    Update py_test.go

--- py_test.go
+++ py_test.go
@@ -5,14 +5,26 @@ import (
 	"testing"
 )
 
-func TestPy(t *testing.T) {
-	res, _ := RunPyDef(`
+var pystr = `
 def main(a,b):
-	return {"name":b["name"]}
-		`, "main", []any{1, map[string]any{"name": "hello word!"}})
-	fmt.Println(string(res))
+    import time
+    return a+b+add_bb(12,14)
+`
+var pystr1 = `
+def add_bb(a):
+	return a
+`
 
-	res, _ = RunPy(`print('hello python!')`)
-	fmt.Println(string(res))
+func TestPy(t *testing.T) {
+	p := NewPy()
+	//p.Timeout=time.Second*20
+	p.Func("add_bb", func(call PyFunctionCall) PyValue {
+		v1, _ := call.Argument(0)
+		v2, _ := call.Argument(1)
+		f1 := v1.Float64()
+		f2 := v2.Float64()
+		return PyValue{f1 + f2}
+	})
+	bs, e := p.Run(pystr, "main", []any{1, 2})
+	fmt.Println(string(bs), e)
 }
-
